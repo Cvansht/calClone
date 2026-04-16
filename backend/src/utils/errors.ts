08f117dbbcdcfdcad8125d36e58d147a9436b3d7
Cvansht <shivanshsandilya10@gmail.com>
2026-04-16 18:22:51 +0530

    prepare backend for render deploy

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
 export class AppError extends Error {
-  public readonly statusCode: Number;
+  public readonly statusCode: number;
   public readonly isOperational: boolean;
   constructor(message: string, statusCode = StatusCodes.INTERNAL_SERVER_ERROR) {
     super(message);
