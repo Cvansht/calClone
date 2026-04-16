e09cf5f5439a88d4eaad95bb74873320dcc080ce
Cvansht <shivanshsandilya10@gmail.com>
2026-04-16 18:13:00 +0530

    fixed  TypeError

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
 export class AppError extends Error {
-  public readonly statusCode: number;
+  public readonly statusCode: Number;
   public readonly isOperational: boolean;
   constructor(message: string, statusCode = StatusCodes.INTERNAL_SERVER_ERROR) {
     super(message);
